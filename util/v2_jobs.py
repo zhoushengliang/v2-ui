28017043be8443929eb31ac455851e6ccb94a875
haoyaxuan <haoyaxuan@zto.com>
2021-06-16 09:13:30 +0800

    去掉总流量阈值功能

--- util/v2_jobs.py
+++ util/v2_jobs.py
@@ -108,7 +108,7 @@ def init():
     schedule_job(check_v2_config_job, config.get_v2_config_check_interval())
     schedule_job(traffic_job, config.get_traffic_job_interval())
     schedule_job(check_v2ay_alive_job, 40)
-    schedule_job(total_traffic_thredsold_job, 60)
+    # schedule_job(total_traffic_thredsold_job, 60)
     reset_day = config.get_reset_traffic_day()
     if reset_day <= 0:
         return
